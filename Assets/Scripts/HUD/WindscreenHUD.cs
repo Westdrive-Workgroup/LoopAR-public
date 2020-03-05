1e19f0eb16276e0ab66759505d4c71ca4954ded7
maxpingel <50764902+maxpingel@users.noreply.github.com>
2020-03-05 23:22:14 +0100

    new windshieldstuff by max

--- Assets/Scripts/HUD/WindscreenHUD.cs
+++ Assets/Scripts/HUD/WindscreenHUD.cs
@@ -9,9 +9,11 @@ public class WindscreenHUD : MonoBehaviour
     public Text MaxSpeed;
     public Text Date;
     public Text Weather;
+    public int Speedlimit;
     public GameObject Car;
     private Rigidbody RB;
     private int nextUpdate = 1;
+    public bool Event;
 
 
     // Start is called before the first frame update
@@ -38,15 +40,25 @@ public class WindscreenHUD : MonoBehaviour
         // Update is called once per second
         void UpdateEverySecond()
         {
+            if(Event == false){
             //Speed
             var Velocity = RB.velocity;
             float speed = Velocity.magnitude;
             Speed.text = Mathf.Round(speed * 3) + "km/h";
             
             //Datum
+            var today = System.DateTime.Now;
+            Date.text = today.ToString("HH:mm");
             //MaxSpeed
-            
+            MaxSpeed.text = Speedlimit + "km/h";
             //Weather
+            Weather.text = "Baumles, CH" + "\n" + "15°C" +"\n" + "Teilweise Bewölkt";
+            }else{
+                Speed.text = "";
+                Date.text = "";
+                MaxSpeed.text = "";
+                Weather.text = "";
+            }
 
 
         }
