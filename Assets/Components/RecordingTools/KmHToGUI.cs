5505234a4fdba635b48271b7732b0587bc8f533c
Linus Tiemann <linus-tiemann@live.de>
2020-03-13 14:26:09 +0100

    forgotten references
    renamed also in the recording tools. Note these are still very early, and obviously need some refinement

--- Assets/Components/RecordingTools/KmHToGUI.cs
+++ Assets/Components/RecordingTools/KmHToGUI.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 public class TextToGUI : MonoBehaviour
 {
     [SerializeField] private Text _text;
-    [SerializeField] private CarControl _carControl;
+    [SerializeField] private CarController _carControl;
     private void Update()
     {
         _text.text= "Km/h: " + Math.Round((_carControl.GetCurrentSpeed()*3.6f),2);
