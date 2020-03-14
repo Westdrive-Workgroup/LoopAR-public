e38ff984ed9a10639462957ab3af29b08f98d22f
Linus Tiemann <linus-tiemann@live.de>
2020-03-14 15:30:45 +0100

    added Registations to EventManager
    Cars with the EventBehavior Script will be Registered to the TrafficEventmanager directly when they are instantiated.
    
    The Eventmanager uses a Singleton Pattern.
    
    Currently a Delegate system seems not to be necessary.
    
    I also added some visual triggers to the scene.
    
    I fixed a rider bug which seem to make problems

---
+++ Assets/Scripts/Cars/EventSystem/EventBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PersistentTrafficEventManager.Instance.RegisterTrafficListeners(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Cars/EventSystem/PersistentTrafficEventManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentTrafficEventManager : MonoBehaviour
+{
+    public static PersistentTrafficEventManager Instance { get; private set; }
+
+    private List<EventBehavior> _eventBehaviorListeners;
+
+    // Start is called before the first frame update
+    
+    private void Awake()
+    {
+        _eventBehaviorListeners = new List<EventBehavior>();
+        //singleton pattern a la Unity
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);         //the Traffic Manager should be persitent by changing the scenes maybe change it on the the fly
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void Start()
+    {
+        // 
+    }
+
+    public void RegisterTrafficListeners(EventBehavior listener)
+    {
+        _eventBehaviorListeners.Add(listener);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
