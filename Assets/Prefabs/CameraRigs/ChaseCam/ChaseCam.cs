b6cc5a26765317ff52769393c5005985cc745041
Linus Tiemann <linus-tiemann@live.de>
2020-03-19 20:18:02 +0100

    refined folder Structure
    added the new Folders and deleted unnessary stuff

--- Assets/Prefabs/CameraRigs/ChaseCam/ChaseCam.cs
+++ Assets/Prefabs/CameraRigs/ChaseCam/ChaseCam.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
-public class CameraFollow : MonoBehaviour
+public class ChaseCam : MonoBehaviour
 {
     public GameObject objectToFollow;
+    [Range(0f, 10f)] public float damping;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    void Update()
+    
+    
+    
+    
+    private void LateUpdate()
     {
         this.transform.position = objectToFollow.transform.position;
+        
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, objectToFollow.transform.rotation,
+            Time.deltaTime * damping);
     }
     // Update is called once per frame
     
