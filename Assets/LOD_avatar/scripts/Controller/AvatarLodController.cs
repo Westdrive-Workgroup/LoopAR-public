f22945fefdd3018b656ce8d892900b55c5a652c9
Phil <spaniolphilipp@gmail.com>
2020-03-25 19:44:59 +0100

    added pedestrian070, added basic animations (with model)

---
+++ Assets/LOD_avatar/scripts/Controller/AvatarLodController.cs
@@ -0,0 +1,37 @@
+﻿using LOD_avatar.scripts.Manager;
+using UnityEngine;
+
+namespace LOD_avatar.scripts.Controller
+{
+    public class AvatarLodController : MonoBehaviour
+    {
+        private LODGroup lodg;
+
+        //LOD[] lods = new LOD[4];
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            lodg = gameObject.GetComponent<LODGroup>();
+
+            LOD[] updateLods = new LOD[lodg.GetLODs().Length];
+            for (int lodLevel = 0; lodLevel < lodg.GetLODs().Length; lodLevel++)
+            {
+                LOD loD = new LOD
+                {
+                    screenRelativeTransitionHeight = AvatarLodManager.instance.GetSpecificLodValue(lodLevel),
+                    renderers = lodg.GetLODs()[lodLevel].renderers
+                };
+                updateLods[lodLevel] = loD;
+            }
+
+            lodg.SetLODs(updateLods);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            lodg.fadeMode = AvatarLodManager.instance.GetFadeMode();
+        }
+    }
+}
