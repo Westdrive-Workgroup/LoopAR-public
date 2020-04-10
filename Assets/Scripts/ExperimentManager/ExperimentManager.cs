00ff2fb08159e5ecd85848aa4d2ed8be9c396bed
Nora <noora.maleki@gmail.com>
2020-04-10 15:46:04 +0200

    Added functionalities to ExperimentManager
    + ExperimentManager Prefab

--- Assets/Scripts/ExperimentManager/ExperimentManager.cs
+++ Assets/Scripts/ExperimentManager/ExperimentManager.cs
@@ -7,10 +7,31 @@ using UnityEngine;
 public class ExperimentManager : MonoBehaviour
 {
     public static ExperimentManager Instance { get; private set; }
-    public GameObject participantsCar;
+    [SerializeField] private GameObject participantsCar;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Camera _firstPersonCamera;
+
+    // registers in which scene or state the experiment is
+    private enum Scene
+    {
+        MainMenu,
+        CountryRoad,
+        MountainRoad,
+        Autobahn,
+        City,
+        EndOfExperiment
+    }
+    
+    // todo use them
+    private enum Event
+    {
+        Deer,
+        BrokenCar
+    }
+    
+    private Scene _scene;
     
     private List<ActivationTrigger> _activationTriggers;
-
 
     private void Awake()
     {
@@ -31,43 +52,83 @@ public class ExperimentManager : MonoBehaviour
 
     void Start()
     {
-        participantsCar.SetActive(false);
-        
-        // inform all triggers to disable their gameobjects at the beginning of the experiment
+        RunMainMenu();
+        InformTriggers();
+    }
+
+    
+    private void Update()
+    {
+        if (_scene == Scene.EndOfExperiment)
+        {
+            InformTriggers();
+            participantsCar.SetActive(false);
+        }
+    }
+
+
+    // inform all triggers to disable their gameobjects at the beginning of the experiment
+    private void InformTriggers()
+    {
         foreach (var trigger in _activationTriggers)
         {
             trigger.DeactivateTheGameObjects();
         }
     }
 
-    // Update is called once per frame
 
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            participantsCar.SetActive(true);   
-        }
+    // main menu
+    private void RunMainMenu()
+    { 
+        _scene = Scene.MainMenu;
+        
+        participantsCar.SetActive(false);
+        _firstPersonCamera.enabled = false;
+        _camera.transform.position = Vector3.zero;
+        _camera.transform.rotation = Quaternion.Euler(0,-90,0); 
     }
     
-    
-    // main menu
+
+    public void OnGUI()
+    {
+
+        if (_scene == Scene.MainMenu)
+        {
+            if (GUI.Button(new Rect(500, 210, 200, 30), "Validation and Calibration"))
+            {
+                //todo call eye tracker validation and find a good name for this button
+                Debug.Log("Clicked validation and calibration");
+            }
+
+            if (GUI.Button(new Rect(500, 250, 200, 30), "Start the experiment"))
+            {
+                StartExperiment();
+            }
+        }
+        else
+        {
+            GUI.enabled = false;
+        }
+    }
 
 
-    // Reception desk for classes to register themselves
+    // Reception desk for ActivationTriggers to register themselves
     public void RegisterToExperimentManager(ActivationTrigger listner)
     {
         _activationTriggers.Add(listner);
     }
 
-    // start experiment
+    
+    // starting the experiment
     private void StartExperiment()
     {
+        _scene = Scene.CountryRoad;
+        _camera.enabled = false;
+        _firstPersonCamera.enabled = true;
         participantsCar.SetActive(true);
     }
-
-    // calibration
     
-    // End experiment
+    //todo inform the controllers
+    //todo call data saving (maybe)
     
 }
