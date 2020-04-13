62a2e96f4113cc884abf59b26b1d2081e4f51f76
Nora <noora.maleki@gmail.com>
2020-04-13 14:46:31 +0200

    Refined main menu function and called data saving. It should be activated though when the function is implemented

--- Assets/Scripts/ExperimentManager/ExperimentManager.cs
+++ Assets/Scripts/ExperimentManager/ExperimentManager.cs
@@ -9,12 +9,15 @@ using Valve.VR;
 public class ExperimentManager : MonoBehaviour
 {
     public static ExperimentManager Instance { get; private set; }
+    
+    [Space] [Header("Necessary Objects")]
     [SerializeField] private GameObject participantsCar;
     [SerializeField] private Camera _camera;
     [SerializeField] private Camera firstPersonCamera;
-    [SerializeField] private int labelFontSize = 20;
-    
-    
+
+
+
+
     // registers in which scene or state the experiment is
     private enum Scene
     {
@@ -69,16 +72,7 @@ public class ExperimentManager : MonoBehaviour
             Debug.Log("<color=red>Error: </color>EyetrackingManager should be present in the scene.");
         }
     }
-
     
-    private void Update()
-    {
-        if (_scene == Scene.EndOfExperiment)
-        {
-            
-        }
-    }
-
 
     // main menu
     private void RunMainMenu()
@@ -101,30 +95,87 @@ public class ExperimentManager : MonoBehaviour
     }
 
 
+    // Reception desk for ActivationTriggers to register themselves
+
+    public void RegisterToExperimentManager(ActivationTrigger listener)
+    {
+        _activationTriggers.Add(listener);
+    }
+
+
+    // starting the experiment
+
+    private void StartExperiment()
+    {
+        _scene = Scene.CountryRoad;
+        _camera.enabled = false;
+        firstPersonCamera.enabled = true;
+        participantsCar.SetActive(true);
+    }
+
+    // ending the experiment
+    public void EndTheExperiment()
+    {
+        //todo activate data saving
+        // EyetrackingManager.Instance.DataSaving();
+        FadeOut();
+        participantsCar.SetActive(false);
+    }
+
+
+    // used for respawning the participant car in case of screwing up
+    private void FadeIn()
+    {
+        SteamVR_Fade.Start(Color.clear, 2f);
+    }
+
+
+    // usage: in case the participant screws up and end of the experiment
+    private void FadeOut()
+    {
+        SteamVR_Fade.Start(Color.black, 2f);
+    }
+
     public void OnGUI()
     {
+        float height = Screen.height;
+        float width = Screen.width;
+        
+        float xForButtons = (width / 2f) - 100f;
+        float yForButtons = height / 2f;
+        
+        float xForLable = (Screen.width / 2f) - 250f;
+        float yForLable = yForButtons - (height / 2.5f);
+
+        float buttonWidth = 200f;
+        float buttonHeight = 30f;
+        float heightDifference = 40f;
+        
+        int labelFontSize = 33;
+
         if (_scene == Scene.MainMenu)
         {
             // Lable
             GUI.color = Color.black;
             GUI.skin.label.fontSize = labelFontSize;
             GUI.skin.label.fontStyle = FontStyle.Bold;
-            GUI.Label(new Rect(450, 40, 500, 100),  "Welcome to Westdrive LoopAR");
+            GUI.Label(new Rect(xForLable, yForLable, 500, 100),  "Welcome to Westdrive LoopAR");
             
             // Buttons
             GUI.backgroundColor = Color.blue;
             GUI.color = Color.white;
-            if (GUI.Button(new Rect(500, 170, 200, 30), "Calibration"))
+            
+            if (GUI.Button(new Rect(xForButtons, yForButtons - heightDifference, buttonWidth, buttonHeight), "Calibration"))
             {
                 EyetrackingManager.Instance.StartCalibration();
             }
 
-            if (GUI.Button(new Rect(500, 210, 200, 30), "Validation"))
+            if (GUI.Button(new Rect(xForButtons, yForButtons, buttonWidth, buttonHeight), "Validation"))
             {
                 EyetrackingManager.Instance.StartValidation();
             }
 
-            if (GUI.Button(new Rect(500, 250, 200, 30), "Start the experiment"))
+            if (GUI.Button(new Rect(xForButtons, yForButtons + heightDifference, buttonWidth, buttonHeight), "Start the experiment"))
             {
                 StartExperiment();
             }
@@ -134,39 +185,4 @@ public class ExperimentManager : MonoBehaviour
             GUI.enabled = false;
         }
     }
-
-
-    // Reception desk for ActivationTriggers to register themselves
-    public void RegisterToExperimentManager(ActivationTrigger listener)
-    {
-        _activationTriggers.Add(listener);
-    }
-
-    
-    // starting the experiment
-    private void StartExperiment()
-    {
-        _scene = Scene.CountryRoad;
-        _camera.enabled = false;
-        firstPersonCamera.enabled = true;
-        participantsCar.SetActive(true);
-    }
-
-    private void FadeIn()
-        {
-            SteamVR_Fade.Start(Color.clear, 2f);
-        }
-        
-        private void FadeOut()
-        {
-            SteamVR_Fade.Start(Color.black, 2f);
-            
-        }
-
-        public void EndTheExperiment()
-        {
-            //todo call data saving
-            FadeOut();
-            participantsCar.SetActive(false);
-        }
 }
