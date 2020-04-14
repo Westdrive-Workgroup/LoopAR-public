4cc79218b6c93562ac178c6c8ca76d01020a4ad2
maxpingel <50764902+maxpingel@users.noreply.github.com>
2020-04-14 13:56:43 +0200

    Working HUD

--- Assets/Scripts/HUD/WindscreenHUD.cs
+++ Assets/Scripts/HUD/WindscreenHUD.cs
@@ -19,23 +19,42 @@ public class WindscreenHUD : MonoBehaviour
     private float speed;
     public GameObject KameraHalt;
     List<GameObject> currentCollider = new List<GameObject>();
+    List<string> objectLinks = new List<string>();
+    List<string> objectRechts = new List<string>();
     public CarController _carController;
     public GameObject KreisFuerEvents;
+    //notMVP
+    //public GameObject PfeilFuersEvent;
     private GameObject Kreis;
+    //notMVP
+    //private GameObject Pfeil;
+    private int p;
     // Start is called before the first frame update
     void Start()
     {
     }
     private void OnTriggerEnter(Collider other)
     {
-        //Event start
-        Event = true;
-        NonEventAnzeigen.SetActive(false);
+        if (Event == false)
+        {
+            //Event start
+            Event = true;
+            NonEventAnzeigen.SetActive(false);
+        }
+
         if (currentCollider.Contains(other.gameObject) == false)
         {
             currentCollider.Add(other.gameObject);
             Kreis = Instantiate(KreisFuerEvents);
             Kreis.transform.SetParent(other.gameObject.transform, false);
+            //notMVP
+            //Pfeil = Instantiate(PfeilFuersEvent);
+            //notMVP
+            //Pfeil.transform.SetParent(Kreis.transform, false);
+            //notMVP
+            //Pfeil.SetActive(false);
+
+
         }
     }
 
@@ -49,6 +68,9 @@ public class WindscreenHUD : MonoBehaviour
         {
             // we have children!
             Destroy(other.transform.Find("Magical_Circle(Clone)").gameObject);
+            if (objectLinks.Count == 0) { linkeSeite.SetActive(false); }
+            if (objectRechts.Count == 0) { rechteSeite.SetActive(false); }
+
             //not any more 
         }
 
@@ -56,7 +78,7 @@ public class WindscreenHUD : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // If the next update is reached
         if (Time.time >= nextUpdate)
@@ -71,49 +93,111 @@ public class WindscreenHUD : MonoBehaviour
         if (currentCollider.Count == 0)
         {
             Event = false;
-
         }
         else
         {
             foreach (GameObject gObject in currentCollider)
             {
                 Vector3 direction = gObject.transform.position - KameraHalt.transform.position;
+                //Vector3 direction_arrow = KameraHalt.transform.forward;
                 RaycastHit hit;
-                // Does the ray intersect any objects excluding the player layer and object has a child
+                RaycastHit hit_arrow;
+
+                // Does the ray intersect any objects  and object has a child
                 if (Physics.Raycast(KameraHalt.transform.position, direction, out hit, Mathf.Infinity) && (gObject.transform.childCount > 0))
                 {
                     switch (hit.collider.gameObject.name)
                     {
+                        //make sure that Event collider layer is on IgnoreRaycast!!!
                         case "Canvas":
                             gObject.transform.GetChild(0).gameObject.SetActive(true);
                             float dist = Vector3.Distance(gObject.transform.position, hit.point);
-                            float quot =(5/dist)+1;
-                            Debug.Log(quot + " dist =" + dist);
+                            float quot = (5 / dist) + 1;
                             //set Kreis position and rotation
                             Kreis = gObject.transform.Find("Magical_Circle(Clone)").gameObject;
                             Kreis.transform.position = hit.point;
-                            Kreis.transform.localScale = new Vector3(0.03f*quot, 0.03f*quot, 1f);
-                            Kreis.transform.rotation = new Quaternion(-50f, -180f, 130f, 1f);  
+                            Kreis.transform.localScale = new Vector3(0.03f * quot, 0.03f * quot, 1f);
+                            Kreis.transform.rotation = Quaternion.Euler(hit.collider.gameObject.transform.rotation.x, hit.collider.gameObject.transform.rotation.y,
+                             hit.collider.gameObject.transform.rotation.z);
 
-                            linkeSeite.SetActive(false);
-                            rechteSeite.SetActive(false);
+
+                            //Wenn das object vor dir ist, ist es nicht hinter dir
+                            if (objectLinks.Count == 0) { linkeSeite.SetActive(false); }
+                            if (objectRechts.Count == 0) { rechteSeite.SetActive(false); }
+                            if (objectLinks.Contains(gObject.gameObject.name) == true)
+                            {
+                                objectLinks.Remove(gObject.gameObject.name);
+                            }
+                            if (objectRechts.Contains(gObject.gameObject.name) == true)
+                            {
+                                objectRechts.Remove(gObject.gameObject.name);
+                            }
+                            //notMVP
+                            //
+                            //if (Physics.Raycast(KameraHalt.transform.position,Vector3.LerpUnclamped(KameraHalt.transform.forward,direction,0.015f), out hit_arrow, Mathf.Infinity))
+                            //{
+                            // float distancePoints=Vector3.Distance(hit_arrow.point, hit.point) ;
+                            // if (distancePoints > 0.5f)
+                            // {Pfeil.SetActive(true);
+                            //     Debug.Log(distancePoints);
+
+
+                            //     Pfeil = Kreis.transform.Find("PfeilFuerEvent(Clone)").gameObject;
+                            //     float x = gObject.gameObject.transform.localScale.x;
+                            //     float y = gObject.gameObject.transform.localScale.y;
+                            //     float z = gObject.gameObject.transform.localScale.z;
+                            //     //Pfeil.transform.localScale =new Vector3(Pfeil.transform.localScale.x/x+0.1f,Pfeil.transform.localScale.y/y+0.1f,Pfeil.transform.localScale.z/z+0.1f);
+
+                            //     float step = speed * Time.deltaTime; // calculate distance to move
+                            //     Pfeil.transform.position = Vector3.MoveTowards(hit_arrow.point, hit.point, step);
+
+                            //     Pfeil.transform.forward = hit.point - hit_arrow.point;
+                            // }else{
+                            // Pfeil.SetActive(false);
+                            // }
+
+
+
+
+
                             break;
                         case "linkeSeite":
                             gObject.transform.GetChild(0).gameObject.SetActive(false);
-                            linkeSeite.SetActive(true);
+
+                            if (objectLinks.Contains(gObject.gameObject.name) == false)
+                            {
+                                objectLinks.Add(gObject.gameObject.name);
+                            }
+                            if (objectLinks.Count > 0)
+                            {
+                                linkeSeite.SetActive(true);
+                            }
                             break;
                         case "rechteSeite":
                             gObject.transform.GetChild(0).gameObject.SetActive(false);
-                            rechteSeite.SetActive(true);
+                            //Debug.Log("Rechts ist was vor der Liste");
+                            if (objectRechts.Contains(gObject.gameObject.name) == false)
+                            {
+                                objectRechts.Add(gObject.gameObject.name);
+                                //Debug.Log(objectRechts+ "   neues Object rechts"+ gObject.gameObject.name);
+                            }
+                            if (objectRechts.Count > 0)
+                            {
+                                rechteSeite.SetActive(true);
+                                //Debug.Log("Rechts ist was");
+                            }
+
                             break;
                         default:
-                            gObject.transform.GetChild(0).gameObject.SetActive(false);
-                            linkeSeite.SetActive(false);
-                            rechteSeite.SetActive(false);
-                            //Debug.Log("Improper command given!");
+
+                            if (objectLinks.Count == 0) { linkeSeite.SetActive(false); }
+                            if (objectRechts.Count == 0) { rechteSeite.SetActive(false); }
+
                             break;
                     }
                 }
+
+
                 else
                 {
                     if (gObject.transform.childCount > 0)
@@ -122,7 +206,9 @@ public class WindscreenHUD : MonoBehaviour
 
                     }
                 }
-                Debug.DrawRay(KameraHalt.transform.position, direction * hit.distance, Color.yellow);
+                Debug.DrawRay(KameraHalt.transform.position, KameraHalt.transform.forward * hit.distance * 20, Color.yellow);
+                Debug.DrawRay(KameraHalt.transform.position, direction * hit.distance * 20, Color.green);
+                Debug.DrawRay(KameraHalt.transform.position, Vector3.LerpUnclamped(KameraHalt.transform.forward, direction, 0.015f) * hit.distance * 20, Color.red);
             }
 
         }
@@ -135,7 +221,7 @@ public class WindscreenHUD : MonoBehaviour
 
         if (Event == false)
         {
-            NonEventAnzeigen.SetActive(true);            
+            NonEventAnzeigen.SetActive(true);
             linkeSeite.SetActive(false);
             rechteSeite.SetActive(false);
             speed = Mathf.Round(_carController.GetCurrentSpeedInKmH());
@@ -162,7 +248,7 @@ public class WindscreenHUD : MonoBehaviour
             Date.text = today.ToString("HH:mm");
             //MaxSpeed
             MaxSpeed.text = speedLimit + "";
-            Weather.text = "Baumles, CH \n 15°C";
+            Weather.text = "Westbrück, CH \n 22°C";
 
 
         }
