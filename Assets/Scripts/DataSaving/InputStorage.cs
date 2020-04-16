6f10eee5902f7524b36ce167fcd6b5cbc004f94f
Linus Tiemann <linus-tiemann@live.de>
2020-04-16 15:55:58 +0200

    created some scripts for Input saving

---
+++ Assets/Scripts/DataSaving/InputStorage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputStorage : MonoBehaviour
+{
+
+    
+    
+    public void OnClicked(Button button)
+    {
+        Debug.Log(button.name);
+    }
+    
+    
+    //records all Inputs 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        string keyPressed = Input.inputString;
+
+        Debug.Log(keyPressed);
+   
+    }
+}
