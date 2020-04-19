75fa3a1bf5ef779bdcec2b10f7e3009c02b6dc36
Linus Tiemann <linus-tiemann@live.de>
2020-04-19 15:44:51 +0200

    added HUDLite
    some phil stuff which seems to be updated

--- Assets/Scripts/HUD/HUDLite.cs
+++ Assets/Scripts/HUD/HUDLite.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class HUDLite : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<GameObject> EventObjectsToMark;
+
+    public GameObject HighlightSymbol;
+    
+    
+    public void ActivateHUD(GameObject testAccidentSubject)
     {
-        
+        List<GameObject> ObjectToMark = new List<GameObject>();
+        ObjectToMark.Add(testAccidentSubject);
+        ActivateHUD(ObjectToMark);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ActivateHUD(List<GameObject> testAccidentSubjects)
     {
-        
+        MarkObjects();
     }
+
+    public void DeactivateHUD()
+    {
+        EventObjectsToMark.Clear();
+    }
+
+
+    private void MarkObjects()
+    {
+        foreach (var eventObject in EventObjectsToMark)
+        {
+            HighlightSymbol.transform.SetParent(eventObject.transform);
+        }
+    }
+    
+    
 }
