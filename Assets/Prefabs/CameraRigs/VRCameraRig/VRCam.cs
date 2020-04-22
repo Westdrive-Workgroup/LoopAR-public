7ee489e8f2455fb094c4bf8c5bd7549a3c37d7e3
Linus Tiemann <linus-tiemann@live.de>
2020-04-22 21:14:44 +0200

    finalized first itteration of SeatCalibration

--- Assets/Prefabs/CameraRigs/VRCameraRig/VRCam.cs
+++ Assets/Prefabs/CameraRigs/VRCameraRig/VRCam.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 
 public class VRCam : MonoBehaviour
 {
+    
     public GameObject seatPosition;
+    public bool UseCalibrationOffset;
     private bool SeatActivated;
+    private GameObject _camera;
+    
     private Vector3 _formerPosition;
+
+    private Vector3 CalibrationOffset;
+
+    private void Awake()
+    {
+        CalibrationOffset.x = PlayerPrefs.GetFloat("_hmd_offset_x");
+        CalibrationOffset.y = PlayerPrefs.GetFloat("_hmd_offset_y");
+        CalibrationOffset.z = PlayerPrefs.GetFloat("_hmd_offset_z");
+        
+        _camera= this.transform.GetChild(0).gameObject;
+        if (UseCalibrationOffset)
+        {
+            _camera.transform.position = CalibrationOffset;
+        }
+    }
+
     private void Start()
     {
+        CalibrationOffset = new Vector3();
         SeatActivated = false;
         _formerPosition = new Vector3();
+        _camera= this.transform.GetChild(0).gameObject;
+        
+       
+        _camera.transform.localPosition = CalibrationOffset;
+
     }
 
     private void LateUpdate()
     {
+        
         if (SeatActivated)
         {
             transform.SetPositionAndRotation(seatPosition.transform.position,seatPosition.transform.rotation);
@@ -40,6 +67,11 @@ public class VRCam : MonoBehaviour
     {
         transform.position = _formerPosition;
     }
-    
-    
+
+    public GameObject GetCamera()
+    {
+        return this.transform.GetChild(0).gameObject;
+    }
+
+
 }
