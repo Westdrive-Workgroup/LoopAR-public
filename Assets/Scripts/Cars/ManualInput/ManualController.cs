e31e931d805c87287cbd0179d1a2468ecabf5cbe
Linus Tiemann <linus-tiemann@live.de>
2020-06-24 15:57:39 +0200

    created Car test scripts and prepare for new input system

--- Assets/Scripts/Cars/ManualInput/ManualController.cs
+++ Assets/Scripts/Cars/ManualInput/ManualController.cs
@@ -8,6 +8,7 @@ public class ManualController : MonoBehaviour
 {
     private CarController _carController;
     private bool _manualDriving = false;
+    private bool toggleReverse;
 
     public delegate void OnReceivedInput(float steeringInput, float accelerationInput, float brakeInput);
     public event OnReceivedInput NotifyInputObservers;
@@ -30,10 +31,20 @@ public class ManualController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float brakeInput = Input.GetAxis("Fire2");
-        float accelerationInput = Input.GetAxis("XOne_Trigger");    // W or Arrow up acceleration forward or backwards.
+        
+        float brakeInput = Input.GetAxis("XOne_Trigger Left");
+        float accelerationInput = Input.GetAxis("XOne_Trigger Right");    // W or Arrow up acceleration forward or backwards.
         float steeringInput = Input.GetAxis("Horizontal");    //A or D steering
+        float reverse = Input.GetAxis("Fire3");
+        if (reverse > 0f)
+        {
+            toggleReverse =! toggleReverse;
+        }
 
+        if (toggleReverse)
+        {
+            accelerationInput = -accelerationInput;
+        }
         NotifyInputObservers?.Invoke(steeringInput, accelerationInput, brakeInput);
         
         if (_manualDriving)
---
+++ Assets/Scripts/Tests/KMH.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using Microsoft.SqlServer.Server;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.Newtonsoft.Json.Utilities;
+
+public class KMH : MonoBehaviour
+{
+    public GameObject Car;
+    private String canvasText;
+
+    private float roundedSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roundedSpeed = Mathf.Round(Car.GetComponent<CarController>().GetCurrentSpeedInKmH());
+        canvasText = roundedSpeed.ToString();
+        GetComponent<TextMeshProUGUI>().text = roundedSpeed + " km/h";
+    }
+}
