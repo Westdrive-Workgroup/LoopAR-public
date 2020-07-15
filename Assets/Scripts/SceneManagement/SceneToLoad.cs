15180769f675c78336b2129f95a6dc52abe1a7ed
Nora <noora.maleki@gmail.com>
2020-07-15 20:38:08 +0200

    Added scene to load script

---
+++ Assets/Scripts/SceneManagement/SceneToLoad.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneToLoad : MonoBehaviour
+{
+    public enum Scenes
+    {
+        Westbrueck,
+        Countryroad,
+        Autobahn,
+        MainMenu
+    }
+
+    private GameObject _currentTarget;
+    public Scenes sceneToLoad = Scenes.Westbrueck;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _currentTarget)
+            return;
+
+        _currentTarget = other.gameObject;
+
+        if (other.GetComponent<ManualController>() != null)
+        {
+            other.GetComponent<CarWindows>().SetInsideWindowsAlphaChannel(1);
+            other.GetComponent<CarController>().TurnOffEngine();
+            other.GetComponent<AIController>().enabled = false;
+            SceneManager.LoadSceneAsync("Animation");
+            StartCoroutine(LoadScenesAsync(GetTargetScene()));
+        }
+    }
+
+    private String GetTargetScene()
+    {
+        switch (sceneToLoad)
+        {
+            case Scenes.Westbrueck:
+                return "Westbrueck";
+            case Scenes.Countryroad:
+                return "countryroad01";
+            case Scenes.Autobahn:
+                return "Autobahn";
+            case Scenes.MainMenu:
+                return "MainMenu";
+        }
+
+        return null;
+    }
+    
+    IEnumerator LoadScenesAsync(string sceneName)
+    {
+        Debug.Log(sceneName);
+        yield return new WaitForSeconds(5);
+        Debug.Log("Loadsing...");
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
