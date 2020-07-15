7998a37311fe154c6d560438fbbb3a78f7ecfb0c
Nora <noora.maleki@gmail.com>
2020-07-15 20:38:34 +0200

    Wrote a custom editor for scene to load

---
+++ Assets/Editor/SceneToLoadCustomInspector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneToLoad))]
+public class SceneToLoadCustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SceneToLoad scene = (SceneToLoad)target;
+ 
+        scene.sceneToLoad = (SceneToLoad.Scenes)EditorGUILayout.EnumPopup("Scene To Load", scene.sceneToLoad);
+    }
+}
