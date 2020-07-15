951e691ede9f7417d68df3c4f922a9b258d2c3f5
Nora <noora.maleki@gmail.com>
2020-07-15 15:05:27 +0200

    Removed a commit

--- Assets/Scripts/ExperimentManager/ExperimentManager.cs
+++ Assets/Scripts/ExperimentManager/ExperimentManager.cs
@@ -264,7 +264,6 @@ public class ExperimentManager : MonoBehaviour
             
             if (GUI.Button(new Rect(xForButtons*9, yForButtons, buttonWidth, buttonHeight), "End"))
             {
-                // SceneLoader.Instance.AsyncLoad(4);
                 SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
                 _scene = Scene.MainMenu;
             }
