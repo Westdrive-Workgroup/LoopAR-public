3425023c8ada7bd42cfa8238f2cd9373b35616c8
Nora <noora.maleki@gmail.com>
2020-07-19 17:49:46 +0200

    Created scene loading handler script

---
+++ Assets/Scripts/SceneManagement/SceneLoadingHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadingHandler : MonoBehaviour
+{
+    public static SceneLoadingHandler Instance { get; private set; }
+    
+    private void Awake()
+    {
+        //singleton pattern a la Unity
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void SceneChange(string targetScene, Collider participantsCar)
+    {
+        participantsCar.GetComponent<CarWindows>().SetInsideWindowsAlphaChannel(1);
+        participantsCar.GetComponent<CarController>().TurnOffEngine();
+        participantsCar.GetComponent<AIController>().enabled = false;
+        SceneManager.LoadSceneAsync("SceneLoader");
+        StartCoroutine(LoadScenesAsync(targetScene));
+    }
+
+    IEnumerator LoadScenesAsync(string targetScene)
+    {
+        Debug.Log(targetScene);
+        yield return new WaitForSeconds(2);
+        Debug.Log("Loading...");
+        SceneManager.LoadSceneAsync(targetScene);
+    }
+}
