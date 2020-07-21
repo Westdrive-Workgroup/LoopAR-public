aec2dec797874d4c8df29aad8875834f78634427
Nora <noora.maleki@gmail.com>
2020-07-21 18:31:46 +0200

    Added Westbrueck Manager

---
+++ Assets/Scripts/SceneManagement/SceneManagers/WestbrueckManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+using UnityEngine.SceneManagement;
+
+public class WestbrueckManager : MonoBehaviour
+{
+    [SerializeField] private GameObject initialSpawnPoint;
+    [SerializeField] private PathCreator mainCarPath;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    
+    private void  OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ExperimentManager.Instance.SetInitialSpawnPositionAndRotation(initialSpawnPoint.transform.position, initialSpawnPoint.transform.rotation);
+        ExperimentManager.Instance.SetCarPath(mainCarPath);
+    }
+}
