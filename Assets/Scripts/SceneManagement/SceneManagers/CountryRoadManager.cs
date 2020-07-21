d2d8c26580197fb558142fd02c0a687028e859fa
Nora <noora.maleki@gmail.com>
2020-07-21 18:34:08 +0200

    Added country road Manager

---
+++ Assets/Scripts/SceneManagement/SceneManagers/CountryRoadManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+using UnityEngine.SceneManagement;
+
+public class CountryRoadManager : MonoBehaviour
+{
+    [SerializeField] private GameObject initialSpawnPoint;
+    [SerializeField] private PathCreator mainCarPath;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    
+    private void  OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ExperimentManager.Instance.SetInitialSpawnPositionAndRotation(initialSpawnPoint.transform.position, initialSpawnPoint.transform.rotation);
+        ExperimentManager.Instance.SetCarPath(mainCarPath);
+    }
+}
