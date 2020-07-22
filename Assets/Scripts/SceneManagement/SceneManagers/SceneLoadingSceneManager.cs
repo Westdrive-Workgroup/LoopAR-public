ef9f460a72950bdd41a3d0b93d0cda9e1f0d489f
Nora <noora.maleki@gmail.com>
2020-07-22 18:18:10 +0200

    Created script  and prefab for the scene loading scene

---
+++ Assets/Scripts/SceneManagement/SceneManagers/SceneLoadingSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoadingSceneManager : MonoBehaviour
+{
+    public static SceneLoadingSceneManager Instance { get; private set; }
+
+    [SerializeField] private GameObject participantsCar;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public GameObject GetParticipantsCar()
+    {
+        return participantsCar;
+    }
+}
