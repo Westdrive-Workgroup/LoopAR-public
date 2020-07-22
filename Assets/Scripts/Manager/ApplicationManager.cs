ded9efb3fda56a9da6de57e54c666c468689bc34
Nora <noora.maleki@gmail.com>
2020-07-22 12:16:27 +0200

    Added some cases (ongoing)

--- Assets/Scripts/Manager/ApplicationManager.cs
+++ Assets/Scripts/Manager/ApplicationManager.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using LOD_avatar.scripts.Manager;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplicationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ApplicationManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    
+    private void  OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        switch (sceneName)
+        {
+            case "MainMenu":
+                TurnOffSpecificExperimentComponents();
+                this.GetComponent<CarController>().transform.parent.gameObject.SetActive(false);
+                break;
+            case "EyetrackingValidation":
+                TurnOffSpecificExperimentComponents();
+                this.GetComponent<CarController>().transform.parent.gameObject.SetActive(false);
+                break;
+            case "SeatCalibrationScene":
+                TurnOffSpecificExperimentComponents();
+                break;
+            case "TestDrive2.0":
+                TurnOffSpecificExperimentComponents();
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TurnOffSpecificExperimentComponents()
     {
-        
+        this.GetComponent<PersistentTrafficEventManager>().gameObject.SetActive(false);
+        this.GetComponent<ExperimentManager>().gameObject.SetActive(false);
+        this.GetComponent<AvatarLodManager>().gameObject.SetActive(false);
     }
 }
