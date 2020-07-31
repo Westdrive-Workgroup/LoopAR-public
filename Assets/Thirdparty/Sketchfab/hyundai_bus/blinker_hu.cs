5e75d90721b283be57d750f8087bc15f98d4f040
Phil <spaniolphilipp@gmail.com>
2020-07-31 13:12:18 +0200

    created script and car lights

---
+++ Assets/Thirdparty/Sketchfab/hyundai_bus/blinker_hu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class blinker_hu : MonoBehaviour
+{
+    
+    public Material mat;
+    private Color orange = new Color(255f, 26f, 0.0f, a:1.0f);
+    private int counter;
+
+    void Start()
+    {
+        StartCoroutine("Flicker");
+    }
+
+    IEnumerator Flicker()
+    {
+        while (true)
+        {
+            counter++;
+
+            if (counter % 2 == 0)
+            {
+                mat.SetColor("_EmissionColor",  orange * 0.05f);
+            } else
+            {
+                mat.SetColor("_EmissionColor", Color.white * 1.1f);;
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
