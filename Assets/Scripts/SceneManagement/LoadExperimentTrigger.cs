5d4950a34fdca39071db160cb306da2de53c1d61
Nora <noora.maleki@gmail.com>
2020-08-06 15:33:18 +0200

    Added the script

---
+++ Assets/Scripts/SceneManagement/LoadExperimentTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadExperimentTrigger : MonoBehaviour
+{
+    private GameObject _currentTarget;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _currentTarget)
+            return;
+
+        _currentTarget = other.gameObject;
+
+        if (other.GetComponent<ManualController>() != null)
+        {
+            SceneLoadingHandler.Instance.LoadExperimentScenes();
+        }
+    }
+}
