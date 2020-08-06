da7873f22d089b35526077befa27fbcf65315ad5
Nora <noora.maleki@gmail.com>
2020-08-06 21:17:43 +0200

    Created script and prefab for SceneAssetActivationTrigger
    Script is not done

---
+++ Assets/Scripts/SceneManagement/SceneAssetActivationTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class SceneAssetActivationTrigger : MonoBehaviour
+{
+    private GameObject _currentTarget;
+    
+    public enum Scenes
+    {
+        Westbrueck, 
+        CountryRoad, 
+        Autobahn
+    }
+    
+    public Scenes sceneToActivate = Scenes.Westbrueck;
+    
+    void Start()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _currentTarget)
+            return;
+
+        _currentTarget = other.gameObject;
+        
+        if (other.GetComponent<ManualController>() != null)
+        {
+            
+        }
+    }
+}
