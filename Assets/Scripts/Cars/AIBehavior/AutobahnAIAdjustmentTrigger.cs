fec3226c5a7ae0be5aeb832a29851c1d0a447e19
Nora <noora.maleki@gmail.com>
2020-08-26 21:28:55 +0200

    The related script for that trigger

---
+++ Assets/Scripts/Cars/AIBehavior/AutobahnAIAdjustmentTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutobahnAIAdjustmentTrigger : MonoBehaviour
+{
+    private GameObject _currentTarget;
+
+    void Start()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _currentTarget)
+            return;
+
+        _currentTarget = other.gameObject;
+
+        if (other.GetComponent<ManualController>() != null)
+        {
+            other.gameObject.GetComponent<ChangeCarPath>().SetParticipantsCarPath(AutobahnManager.Instance.GetCarPath(), 
+                AutobahnManager.Instance.GetCurveDetectorStepAhead(), 0.008f, 10f);
+        }
+    }
+}
