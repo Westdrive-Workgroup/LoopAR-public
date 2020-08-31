5d7fab1e9a91a89438b26f8392c64fb9c9dfdc59
Frederik <fnienhaus@uos.de>
2020-08-31 11:55:51 +0200

    Training Scene Debugging
    Got rid of some small bugs and made the paths smoother.

--- Assets/Scripts/TestDrive2.0/SwitchControlTrigger.cs
+++ Assets/Scripts/TestDrive2.0/SwitchControlTrigger.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using PathCreation;
+using RoboRyanTron.Unite2017.Variables;
 using UnityEngine;
 
 public class SwitchControlTrigger : MonoBehaviour
@@ -10,21 +11,37 @@ public class SwitchControlTrigger : MonoBehaviour
     [SerializeField] private TestEventManager testEventManager;
     [SerializeField] private float newSpeed;
     [SerializeField] private float delayTillEventDeactivates;
+    
+    [SerializeField] private FloatVariable maxTrials;
+    [SerializeField] private FloatVariable trialsDone;
+    [SerializeField] private FloatVariable timeToWait;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CarController>())
         {
-            testEventManager.DeactivateHUD();
+            StartCoroutine(GiveAIControl(other));
             StartCoroutine(testEventManager.DeactivateEvent(delayTillEventDeactivates));
-            GiveAIControl(other);
+            testEventManager.DeactivateHUD();
         }
     }
 
-    private void GiveAIControl(Collider other)
+    private IEnumerator GiveAIControl(Collider other)
     {
-        other.gameObject.GetComponent<AimedSpeed>().SetRuleSpeed(newSpeed/3.6f);
+        other.gameObject.GetComponent<CarController>().TurnOffEngine();
         other.gameObject.GetComponent<ControlSwitch>().SwitchControl(false);
+
+        if (trialsDone.Value >= maxTrials.Value)
+        {
+            yield return new WaitForSecondsRealtime(timeToWait.Value);
+        }
+        else
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        
+        other.gameObject.GetComponent<AimedSpeed>().SetRuleSpeed(newSpeed/3.6f);
+        other.gameObject.GetComponent<CarController>().TurnOnEngine();
 
         other.gameObject.GetComponent<AIController>().SetNewPath(newPath);
         other.gameObject.GetComponent<AIController>().SetLocalTargetAndCurveDetection();
