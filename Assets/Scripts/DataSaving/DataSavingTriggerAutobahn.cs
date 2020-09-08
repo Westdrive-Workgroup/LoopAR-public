7dd31ca1ae03bb01379a5c09abc96b8e8e137957
Nora <noora.maleki@gmail.com>
2020-09-08 10:54:43 +0200

    Fixed the scene data issue in autobahn

---
+++ Assets/Scripts/DataSaving/DataSavingTriggerAutobahn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSavingTriggerAutobahn : MonoBehaviour
+{
+    private GameObject _currentTarget;
+    
+    void Start()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _currentTarget)
+            return;
+
+        _currentTarget = other.gameObject;
+        
+        if (other.GetComponent<ManualController>() != null)
+        {
+            SavingManager.Instance.StopAndSaveData("Autobahn");
+        }
+    }
+}
