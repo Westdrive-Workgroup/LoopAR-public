1bee7e906558cd0759e6cfa1bd0380d7db684590
Linus T <linus-tiemann@live.de>
2020-09-10 17:52:21 +0200

    Forcefeedback
    It is finally happening. my first , real commit since a long time, since I went through the nomans lands of the internet to find a solution to finally realize Forcefeedback.
    
    Note that it is still buggy, Potentially a second editor run might break it. We will dive into this in the future commits!

---
+++ Assets/Scripts/Cars/ForceFeedback/ForceFeedback.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace ForceFeedback
+{
+    public class FFB 
+    {
+        // Start is called before the first frame update
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetActiveWindow();
+        [DllImport("LibWestDriveFFB.dll")]
+        public static extern int InitDirectInput(IntPtr hDlg);
+        [DllImport("LibWestDriveFFB.dll")]
+        public static extern void FreeDirectInput();
+        [DllImport("LibWestDriveFFB.dll")]
+        private static extern void StopEffect();
+        [DllImport("LibWestDriveFFB.dll")]
+        public static extern void RunEffect();
+        [DllImport("LibWestDriveFFB.dll")]
+        private static extern int SetDeviceForcesXY(int x_Force, int y_Force);
+        [DllImport("LibWestDriveFFB.dll")]
+        public static extern bool AcquireDevice();
+
+        public static void ForceFeedBackInit()
+        {
+            FFB.InitDirectInput(GetActiveWindow());
+        }
+        public static void SetDeviceForceFeedback(int xForce, int yForce)
+        {
+            FFB.SetDeviceForcesXY(xForce, yForce);
+            FFB.RunEffect();
+        }
+
+        public static void StopForceFeedback()
+        {
+            FFB.StopEffect();
+        }
+        
+    }
+}
+
